5deca5bc24944897ba158842b1b12d6dc6ab229d
SamiduSamarasinghe <samarasinghesamidu@gmail.com>
2025-11-13 11:16:51 +0530

    add dependencies

---
+++ personal-taskmanager/src/main/java/com/taskmanager/personal_taskmanager/PersonalTaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.personal_taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTaskmanagerApplication.class, args);
+	}
+
+}
---
+++ personal-taskmanager/src/test/java/com/taskmanager/personal_taskmanager/PersonalTaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.personal_taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
